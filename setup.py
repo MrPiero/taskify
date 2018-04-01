24d9b7baaeff85db15bcf5e08a1097bf57b9102b
Piero Bardelli <pbardelli20@gmail.com>
2018-03-31 20:35:39 -0500

    Taskify v0.3 alpha

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='taskify',
+    version='0.3',
+    description='Taskify manages your tasks of the day, with notes and process.',
+    long_description=long_description,
+    url='https://github.com/MrPiero/taskify',
+    author='Piero Bardelli',
+    author_email='pbardelli20@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6.4',
+    ],
+    keywords='tasks process agenda work',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'venv']),
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'taskify=bin.__main__:main',
+        ],
+    },
+)
